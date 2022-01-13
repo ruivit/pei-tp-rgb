adc7fd9d5e15adf35bc6ae7a8552e4e9bb183798
Rui Vitorino <8190479@estg.ipp.pt>
2022-01-13 18:50:53 +0000

    Added RandomData
    Script & RandomData on DB

--- generateData.py
+++ generateData.py
@@ -1,7 +1,9 @@
 from ctypes.wintypes import PINT
 from math import nan
 from yattag import Doc, indent
-import random, os
+import random, os, re
+import requests, time
+import sys
 
 doc, tag, text = Doc().tagtext()
 
@@ -15,17 +17,16 @@ dates = []
 for i in range(0, 100):
     dates.append(str(random.randint(2022, 2022)) + "-" + str(random.randint(10, 12)) + "-" + str(random.randint(10, 25)))
 
-#generate 10 random names
+#generate random needed data
 names = ["John", "Raul", "Pablo", "Juan", "Pedro", "Carlos", "Jorge", "Luis", "Jose", "Ramon"]
-
 countryNames = ["Spain", "France", "Germany", "Italy", "Portugal", "United Kingdom", "United States of America", "Canada", "Mexico", "Brazil"]
-
 cityNames = ["Madrid", "Paris", "Berlin", "Rome", "Lisbon", "London", "New York", "Ottawa", "Mexico City", "Sao Paulo"]
-
 countryCodes = ["ES", "FR", "DE", "IT", "PT", "GB", "USA", "CA", "MX", "BR"]
 
-# make a xml from the xml variable above
 
+def createData():
+    with open(os.path.join(os.path.dirname(__file__), 'export', 'reservation' + str(i) + '.xml'), 'w') as f:
+        f.write(makeXML(i))
 
 def makeXML(i):
     # make a empty xml
@@ -35,16 +36,13 @@ def makeXML(i):
     with tag('root'):
         with tag('reservation'):
             with tag('f:family'):
-                with tag('f:familyElement'):
-                    with tag('f:name'):
-                        text(random.choice(names))
-                    with tag('f:birthDate'):
-                        text('1992-06-22')
-                with tag('f:familyElement'):
-                    with tag('f:name'):
-                        text(random.choice(names))
-                    with tag('f:birthDate'):
-                        text('1990-05-24')
+                # create a random number of familyElement
+                for j in range(0, random.randint(1, 7)):
+                    with tag('f:familyElement'):
+                        with tag('f:name'):
+                            text(random.choice(names))
+                        with tag('f:birthDate'):
+                            text('1992-06-22')
                 with tag('f:origin', countryCode=random.choice(countryCodes)):
                     with tag('gd:countryName'):
                         text(random.choice(countryNames))
@@ -53,16 +51,78 @@ def makeXML(i):
                 with tag('f:reservationDates'):
                     with tag('f:numberOfDays'):
                         text(str(random.randint(1, 5)))
-                    with tag('f:preferedDates'):
-                        text(random.choice(dates))
-                    with tag('f:preferedDates'):
-                        text(random.choice(dates))
+                    for j in range(0, random.randint(1, 5)):
+                        with tag('f:preferedDates'):
+                            text(random.choice(dates))
     
         # save the xml to a variable
         xml = indent(doc.getvalue())
         return xml
 
-for i in range(1, 101):
-    # save the xml to a file in dir export
-    with open(os.path.join(os.path.dirname(__file__), 'export', 'reservation' + str(i) + '.xml'), 'w') as f:
-        f.write(makeXML(i))
+def POSTreservations():
+    regex = '<reservation>'
+    regex2 = """<?xml version="1.0" encoding="UTF-8"?>
+    <reservation   xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"  
+                    xmlns="http://www.atelierRGB.pt/Reservation" 
+                    xsi:schemaLocation="http://www.atelierRGB.pt/Reservation ../XSD/Reservation.xsd"
+                    
+                    xmlns:f="http://www.atelierRGB.pt/Family"
+                    xmlns:gd="http://www.atelierRGB.pt/GeographicData">"""
+
+    for filename in os.listdir(r'C:\Program Files (x86)\BaseX\webapp\export'):
+        if filename.startswith('reservation'):
+            with open(os.path.join(r'C:\Program Files (x86)\BaseX\webapp\export', filename), 'r') as f:
+                
+                content = f.read()
+
+                content = re.sub(regex, regex2, content)
+
+                url = "http://localhost:8984/makereservation"
+
+                headers = {
+                'Authorization': 'Basic YWRtaW46YWRtaW4=',
+                'Content-Type': 'application/xml'
+                }
+
+                time.sleep(0.05)
+                response = requests.request("POST", url, headers=headers, data=content)
+
+                print(response.text)
+
+def randomCancel():
+    url = "http://localhost:8984/cancelreservation?id=" + str(random.randint(1, 100))
+    
+    time.sleep(0.05)
+    response = requests.request("GET", url)
+    
+    print(response.text)
+
+def exportData():
+    url = "http://localhost:8984/exportdatabase"
+    
+    response = requests.request("GET", url)
+    
+    print(response.text)
+
+# ------- INIT ------- #
+try:
+    arg = sys.argv[1]
+except IndexError:
+    print("""Script Execution:
+$ python generateData.py <number> <cancel>
+    where <number> is the number of XMLs to create
+    and <cancel> is the number of reservations to cancel""")
+    exit()
+
+# Create XMLs with random data in it
+for i in range(1, int(sys.argv[1]+1)):
+    createData()
+
+# POST it to BaseX
+POSTreservations()
+
+# Cancel some random reservations
+for cancel in range(1, int(sys.argv[2]+1)):
+    randomCancel()
+
+exportData()
