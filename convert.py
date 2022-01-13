610a1242b73ee8261c262abe2ed81b995aeb2dda
onvrb <23731360+onvrb@users.noreply.github.com>
2022-01-13 01:23:38 +0000

    feat parse and import to mongodb

--- convert.py
+++ convert.py
@@ -1,53 +1,100 @@
-import re, os, json, xmltodict
-from pymongo import MongoClient
-import pymongo
-from pymongo import MongoClient
-#teste
-dir = r"PEITP"
+import os, re, json, bson, pymongo
+from bson import json_util
+from pymongo import MongoClient, InsertOne
+from dotenv import load_dotenv
+from glob import glob
+import xmltodict
+
+xml_dir = "EXPORTDB-GoodExamples"
+json_dir = "json"
+json_files = glob(json_dir + "/*.json")
+workshop_collection = []
+
+load_dotenv()
+import_to_mongodb = os.getenv('IMPORT_TO_MONGODB')
+if import_to_mongodb:
+    mongodb_host=os.getenv("MONGODB_HOST")
+    mongodb_dbname=os.getenv("MONGODB_DBNAME")
+    mongodb_user=os.getenv("MONGODB_USER")
+    mongodb_password=os.getenv("MONGODB_PASSWORD")
+
+def clean_xml(content):
+    # remove the namespaces
+    content = re.sub('xmlns(|:xsi|:f)="http.+(?=">)"', '', content)
+    # remove the prefixes
+    content = re.sub('(f:)|(gd:)', '', content)
+    # remove the whitespaces
+    content = re.sub('(?=\s>)\s', '', content)
+    return content
 
 def convert_xml_to_json():
+    for filename in os.listdir(xml_dir):
+        with open(os.path.join(xml_dir, filename), 'r') as f:
+            content = clean_xml(f.read())
+            # save as json
+            with open(os.path.join(json_dir, filename.replace('.xml', '.json')), 'w') as f:
+                xml = xmltodict.parse(content)
 
-    for filename in os.listdir(dir):
-        with open(os.path.join(dir, filename), 'r') as f:
-            content = f.read()
+                if filename.startswith('workshop'):
+                    xml = xml['workshop']['reservations']
+                    global workshop_collection
+                    workshop_collection = xml
+                    # set all workshop_json slots to be the default 50
+                    for dia in workshop_collection:
+                        dia['slots'] = 50
 
-            # remove the namespaces
-            content = re.sub('xmlns(|:xsi|:f)="http.+(?=">)"', '', content)
+                elif filename.startswith('reservation'):
+                    xml = xml['reservation']
+                    # se for array, transforma em dict
+                    if isinstance(xml['family']['familyElement'], dict):
+                        xml['family']['familyElement'] = [xml['family']['familyElement']]
 
-            # remove the prefixes
-            content = re.sub('(f:)|(gd:)', '', content)
+                f.write(json.dumps(xml, indent=4, default=bson.json_util.default))
 
-            # remove the whitespaces
-            content = re.sub('(?=\s>)\s', '', content)
+def entulhar_reservations_in_workshop_collection():
+    # get the reservations json
+    for filename in os.listdir(json_dir):
+        if filename.startswith('reservation'):
+            with open(os.path.join(json_dir, filename), 'r') as f:
+                reservation_json = json.load(f)
 
-            # replace the file and save it
-            with open(os.path.join(dir, filename), 'w') as f:
-                #new_name = filename.replace(".xml", ".json")
-                f.write(json.dumps(xmltodict.parse(content)))
-                f.close()
-            
-def get_database():
-    # Provide the mongodb atlas url to connect python to mongodb using pymongo
-    CONNECTION_STRING = "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
+                # set the workshop_collection slots to be the reservation slots
+                for dia in workshop_collection:
+                    if dia['date'] == reservation_json['date']:
 
-    # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
-    client = MongoClient(CONNECTION_STRING)
+                        # create an array of reservations
+                        if 'reservations' not in dia:
+                            dia['reservations'] = []
+                        
+                        # add reservation in the workshop_collection as array
+                        dia['reservations'].append(reservation_json)
 
-    # Create the database for our example (we will use the same database throughout the tutorial
-    return client['basex']
+                        # remove date from each reservation
+                        reservation_json.pop('date')
+
+                        # remove 1 from slots for each reservation
+                        dia['slots'] -= 1
+                        break
+
+    # save the workshop_collection.json
+    with open(os.path.join(json_dir, 'workshop_collection.json'), 'w') as f:
+        f.write(json.dumps(workshop_collection, indent=4, default=bson.json_util.default))
+
+def get_mongodb(collection_dst):
+    # connection mongodb server
+    client = MongoClient(mongodb_host, username=mongodb_user, password=mongodb_password)
+    # insert to db / create if does not exist
+    return client[mongodb_dbname][collection_dst]
     
-# This is added so that many files can reuse the function get_database()
-if __name__ == "__main__":    
+def import_workshop_json_to_mongodb():
+    collection_dst = 'workshop'
+    mymongodb = get_mongodb(collection_dst)
+    with open(os.path.join(json_dir, 'workshop_collection.json'), 'r') as f:
+        mymongodb.insert_many(json.load(f))
+
+# main
+if __name__ == "__main__":
     convert_xml_to_json()
-
-    dbname = get_database()
-
-    # go trough the json files
-    for filename in os.listdir(dir):
-        with open(os.path.join(dir, filename), 'r') as f:
-            content = f.read()
-
-            # convert the content to a dictionary
-            content = json.loads(content)
-
-            dbname.basex.insert_one(content)
+    entulhar_reservations_in_workshop_collection()
+    if import_to_mongodb:
+        import_workshop_json_to_mongodb()
