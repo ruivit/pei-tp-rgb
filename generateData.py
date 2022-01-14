7d56d3b087846bb9fe6816c70540adfbe97d3922
Rui Vitorino <8190479@estg.ipp.pt>
2022-01-14 18:33:59 +0000

    Quick Check to XSDs and generateData.py

--- generateData.py
+++ generateData.py
@@ -7,23 +7,29 @@ import sys
 
 doc, tag, text = Doc().tagtext()
 
-#generate random dates between 1960-01-01 and 2010-12-31
-birthDates = []
+# create dates between 1990 and 2010 (younger people)
+birthDatesNew = []
 for i in range(0, 100):
-    birthDates.append(str(random.randint(1960, 2010)) + "-" + "0" + str(random.randint(1, 9)) + "-" + str(random.randint(1, 31)))
+    birthDatesNew.append(str(random.randint(1990, 2010)) + "-" + str(random.randint(10, 28)) + "-" + str(random.randint(10, 28)))
 
+# create dates between 1960 and 1990 (older people)
+birthDatesOld = []
+for i in range(0, 100):
+    birthDatesOld.append(str(random.randint(1960, 1990)) + "-" + str(random.randint(10, 28)) + "-" + str(random.randint(10, 28)))
+
+# create random dates between 2022-10-10 and 2022-12-25 for the reservations
 dates = []
-#generate random dates between 2022-10-10 and 2022-12-25
 for i in range(0, 100):
     dates.append(str(random.randint(2022, 2022)) + "-" + str(random.randint(10, 12)) + "-" + str(random.randint(10, 25)))
 
-#generate random needed data
+# create random needed data
 names = ["John", "Raul", "Pablo", "Juan", "Pedro", "Carlos", "Jorge", "Luis", "Jose", "Ramon"]
 countryNames = ["Spain", "France", "Germany", "Italy", "Portugal", "United Kingdom", "United States of America", "Canada", "Mexico", "Brazil"]
 cityNames = ["Madrid", "Paris", "Berlin", "Rome", "Lisbon", "London", "New York", "Ottawa", "Mexico City", "Sao Paulo"]
 countryCodes = ["ES", "FR", "DE", "IT", "PT", "GB", "USA", "CA", "MX", "BR"]
 
 
+# create XMLs with the random generated data (named "reservationX.xml")
 def createData():
     with open(os.path.join(os.path.dirname(__file__), 'export', 'reservation' + str(i) + '.xml'), 'w') as f:
         f.write(makeXML(i))
@@ -42,12 +48,13 @@ def makeXML(i):
                         with tag('f:name'):
                             text(random.choice(names))
                         with tag('f:birthDate'):
-                            text('1992-06-22')
+                            text(random.choice(birthDatesOld))
                 with tag('f:origin', countryCode=random.choice(countryCodes)):
                     with tag('gd:countryName'):
                         text(random.choice(countryNames))
                     with tag('gd:cityName'):
                         text(random.choice(cityNames))
+                # create a random number of reservationDates
                 with tag('f:reservationDates'):
                     with tag('f:numberOfDays'):
                         text(str(random.randint(1, 5)))
@@ -55,7 +62,7 @@ def makeXML(i):
                         with tag('f:preferedDates'):
                             text(random.choice(dates))
     
-        # save the xml to a variable
+        # save the xml to a variable and return it
         xml = indent(doc.getvalue())
         return xml
 
@@ -69,6 +76,7 @@ def POSTreservations():
                     xmlns:f="http://www.atelierRGB.pt/Family"
                     xmlns:gd="http://www.atelierRGB.pt/GeographicData">"""
 
+    # loop through the reservation files and POST it to BaseX
     for filename in os.listdir(r'C:\Program Files (x86)\BaseX\webapp\export'):
         if filename.startswith('reservation'):
             with open(os.path.join(r'C:\Program Files (x86)\BaseX\webapp\export', filename), 'r') as f:
@@ -89,6 +97,7 @@ def POSTreservations():
 
                 print(response.text)
 
+# cancel some random reservations indicated by the cli argument
 def randomCancel():
     url = "http://localhost:8984/cancelreservation?id=" + str(random.randint(1, 100))
     
@@ -97,6 +106,7 @@ def randomCancel():
     
     print(response.text)
 
+# export the database of BaseX
 def exportData():
     url = "http://localhost:8984/exportdatabase"
     
@@ -115,14 +125,14 @@ $ python generateData.py <number> <cancel>
     exit()
 
 # Create XMLs with random data in it
-for i in range(1, int(sys.argv[1]+1)):
+for i in range(1, int(sys.argv[1])):
     createData()
 
 # POST it to BaseX
 POSTreservations()
 
 # Cancel some random reservations
-for cancel in range(1, int(sys.argv[2]+1)):
+for cancel in range(1, int(sys.argv[2])):
     randomCancel()
 
-exportData()
+#exportData()
