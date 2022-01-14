344b755842a18475a70383bc867180df22341ec4
onvrb <23731360+onvrb@users.noreply.github.com>
2022-01-14 00:47:51 +0000

    Create extra.js

---
+++ MONGODB-QUERIES/extra.js
@@ -0,0 +1,23 @@
+/* "Outras visualizações que considerar relevantes."
+
+ideias:
+
+- Dias com mais reservas
+- Dia em que houve mais cancelamentos
+- I
+
+
+
+
+### MONGOCHARTS:
+
+mostrar grafico X axis: dias, Y axis numero de reservas:
+- linha azul reservas ativas e linha vermelha reservas canceladas
+
+chart com grupos etários
+- 0-2 anos, 2-6 anos, 6-12 anos, 12-18 anos, 18-24 anos, 24-30 anos, 30-60 anos, 60+ anos
+
+chart com reservas por país
+
+
+*/
