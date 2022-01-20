b39daef80b4a5793237a93fecc71d928512ae1f9
Rui Vitorino <8190479@estg.ipp.pt>
2022-01-20 19:11:34 +0000

    5k Reservations, and better generateData

--- generateData.py
+++ generateData.py
@@ -1,36 +1,44 @@
+from urllib import response
 from yattag import Doc, indent
 import random, re
 import requests
 import sys, os
+import datetime
 
-# create dates between 1990 and 2010 (younger people)
+# create dates between 1990 and 2020
 birthDatesNew = []
 for i in range(0, 100):
-    birthDatesNew.append(str(random.randint(1990, 2010)) + "-" + str(random.randint(10, 12)) + "-" + str(random.randint(10, 28)))
+    birthDatesNew.append(datetime.date(random.randint(1990, 2020), random.randint(1, 12), random.randint(1, 28)).strftime('%Y-%m-%d'))
+    
 
 # create dates between 1960 and 1990 (older people)
 birthDatesOld = []
 for i in range(0, 100):
-    birthDatesOld.append(str(random.randint(1960, 1990)) + "-" + str(random.randint(10, 12)) + "-" + str(random.randint(10, 28)))
+    birthDatesOld.append(datetime.date(random.randint(1960, 1990), random.randint(1, 12), random.randint(1, 28)).strftime('%Y-%m-%d'))
 
+# create dates between 2012 and 2021 (infants)
 infants = []
 for i in range(0, 100):
-    infants.append(str(random.randint(2012, 2020)) + "-" + str(random.randint(10, 12)) + "-" + str(random.randint(10, 28)))
+    infants.append(datetime.date(random.randint(2000, 2020), random.randint(1, 12), random.randint(1, 28)).strftime('%Y-%m-%d'))
 
-# create random dates between 2022-10-10 and 2022-12-25 for the reservations
+# create random dates between 2022-09-15 and 2022-12-23 for the reservations
 dates = []
 for i in range(0, 100):
-    dates.append(str(random.randint(2022, 2022)) + "-" + str(random.randint(10, 12)) + "-" + str(random.randint(10, 25)))
+    for j in range(0, random.randint(1, 5)):
+        dates.append(datetime.datetime(2022, random.randint(9, 12), random.randint(1, 23)).strftime('%Y-%m-%d'))
 
-# create random needed data
+# create random names for the family elements
 manNames = ["John", "Raul", "Pablo", "Juan", "Pedro", "Carlos", "Jorge", "Luis", "Jose", "Ramon"]
 womanNames = ["Maria", "Marta", "Laura", "Sara", "Ana", "Carmen", "Carla", "Pilar", "Sofia", "Martina"]
+
+# create random phone numbers
 phoneNumbers = ["911922933", "123123123", "944955966", "900900900", "351234567", "256123456", "912341234"]
 
-# create a two dimensional array with the random generated data of countryNames and Cities
-countryNames = ["Spain", "France", "Germany", "Italy", "Portugal", "United Kingdom", "United States of America", "Canada", "Mexico", "Brazil"]
-cityNames = ["Madrid", "Paris", "Berlin", "Rome", "Lisbon", "London", "New York", "Ottawa", "Mexico City", "Sao Paulo"]
+# create country and city data
+countryNames = ["Spain", "France", "Germany", "Italy", "Portugal", "United Kingdom", "United States of America", "Canada", "Brazil"]
+cityNames = [["Madrid", "Toledo", "Badajoz"], ["Paris", "Lyon", "Marseille"], ["Berlin", "Munich", "Frankfurt"],["Rome", "Milan", "Venice"], ["Lisbon", "Porto", "Funchal"], ["London", "Manchester", "Liverpool"],["New York", "Los Angeles", "Chicago"], ["Toronto", "Vancouver", "Montreal"], ["Brasilia", "Rio de Janeiro", "Sao Paulo"]]
 
+# join the country and city data in one array
 countryData = []
 if (len(countryNames) == len(cityNames)):
     for i in range(0, len(countryNames)):
@@ -52,28 +60,30 @@ def makeXML(i):
     with tag('root'):
         with tag('reservation'):
             with tag('f:family'):
-                # create a random number of familyElement
+                # create a random number of familyElements
                 for j in range(0, random.randint(1, 7)):
                     with tag('f:familyElement'):
+                        # half being men and the other half being woman
                         if (random.randint(1, 2) == 1):
                             with tag('f:name'):
                                 text(manNames[random.randint(0, len(manNames)-1)])
-                            #with tag("f:sex"):
-                            #    text("Masculine")
                         else:
                             with tag('f:name'):
                                 text(womanNames[random.randint(0, len(womanNames)-1)])
-                            #with tag("f:sex"):
-                            #    text("Feminine")
-                        if (i == int(sys.argv[1])/4):
+                        
+                        # have 50% chance of at least 1 being a grown up
+                        # have 25% change of being a older person 
+                        # and 25% being an infant
+                        if (random.randint(1, 2) == 1):
                             with tag('f:birthDate'):
-                                text(random.choice(birthDatesOld))
-                            if (i == int(sys.argv[1])/2):
-                                with tag('f:birthDate'):
-                                    text(random.choice(birthDatesNew))
+                                text(birthDatesNew[random.randint(0, len(birthDatesNew)-1)])
+                        elif (random.randint(1, 3) == 1):
+                            with tag('f:birthDate'):
+                                text(birthDatesOld[random.randint(0, len(birthDatesOld)-1)])
                         else:
                             with tag('f:birthDate'):
-                                text(random.choice(infants))
+                                text(infants[random.randint(0, len(infants)-1)])
+
                 with tag('f:emergencyContact'):
                     text(phoneNumbers[random.randint(0, len(phoneNumbers)-1)])
                 with tag('f:origin'):
@@ -81,7 +91,8 @@ def makeXML(i):
                     with tag('gd:countryName'):
                         text(countryData[rInt][0])
                     with tag('gd:cityName'):
-                        text(countryData[rInt][1])
+                        text(countryData[rInt][1][random.randint(0, len(countryData[rInt][1])-1)])
+
                 # create a random number of reservationDates
                 with tag('f:reservationDates'):
                     numberOfDays = random.randint(1, 5)
@@ -123,9 +134,17 @@ def deleteResources():
         for file in os.listdir(r'.\export'):
             os.remove(os.path.join(r'.\export', file))
 
+    # go into JSON folder and remove the files
+    try:
+        for file in os.listdir(r'.\JSON'):
+            os.remove(os.path.join(r'.\JSON', file))
+    except FileNotFoundError:
+        print("Creating directory JSON...")
+        os.makedirs(os.path.join(os.path.dirname(__file__), 'JSON'))
+        for file in os.listdir(r'.\JSON'):
+            os.remove(os.path.join(r'.\JSON', file))
 
 def POSTreservations():
-    liveProbe = 0
     regex = '<reservation>'
     regex2 = """<?xml version="1.0" encoding="UTF-8"?>
     <reservation   xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"  
@@ -136,12 +155,13 @@ def POSTreservations():
                     xmlns:gd="http://www.atelierRGB.pt/GeographicData">"""
 
     # loop through the reservation files and POST it to BaseX
-    for filename in os.listdir(r'C:\Program Files (x86)\BaseX\webapp\export'):
+    for filename in os.listdir(r'.\export'):
         if filename.startswith('reservation'):
-            with open(os.path.join(r'C:\Program Files (x86)\BaseX\webapp\export', filename), 'r') as f:
+            with open(os.path.join(r'.\export', filename), 'r') as f:
                 
                 content = f.read()
 
+                # remove the namespaces and prefixes
                 content = re.sub(regex, regex2, content)
 
                 url = "http://localhost:8984/makereservation"
@@ -151,90 +171,130 @@ def POSTreservations():
                 'Content-Type': 'application/xml'
                 }
 
+                # POST the xml to BaseX
                 try:
                     response = requests.request("POST", url, headers=headers, data=content)
                 except requests.exceptions.ConnectionError:
-                    print("Connection error")
-                    liveProbe += 1
-                    if (liveProbe == 3):
-                        print("\n[!] BaseX seems not to be running [!]\n")
-                        sys.exit()
+                    print("\n[!] BaseX seems not to be running [!]\n")
+                    sys.exit()
 
 
 def randomCancel():
 # cancel some random reservations indicated by the cli argument
-    url = "http://localhost:8984/cancelreservation?id=" + str(random.randint(1, 100))
+    url = "http://localhost:8984/cancelreservation?id=" + str(random.randint(1, int(sys.argv[1])))
     
     response = requests.request("GET", url)
 
 
 def exportData():
-# export the database of BaseX
+# export the database of BaseX to export directory
     url = "http://localhost:8984/exportdatabase"
     
     response = requests.request("GET", url)
     
 
+def initialChecks():
+    # check if the current directory is webapp
+    if 'webapp' not in os.getcwd():
+        print("\n[!] Please run this script from the webapp directory [!]\n")
+        sys.exit()
+
+    # check if the DB and export directories exist
+    try:
+        for file in os.listdir(r'.\DB'):
+            os.remove(os.path.join(r'.\DB', file))
+    except FileNotFoundError:
+        print("DB directory was not found\nCreating it...")
+        os.makedirs(os.path.join(os.path.dirname(__file__), 'DB'))
+        
+    try:
+        for file in os.listdir(r'.\export'):
+            os.remove(os.path.join(r'.\export', file))
+    except FileNotFoundError:
+        print("export directory was not found\nCreating it...")
+        os.makedirs(os.path.join(os.path.dirname(__file__), 'export'))
+
+
+    # send a GET request to the BaseX webpage to check if it is running
+    url = "http://localhost:8984/"
+    try:
+        response = requests.request("GET", url)
+    except requests.exceptions.ConnectionError:
+        print("\n[!] BaseX seems not to be running [!]\n")
+        sys.exit()
+
+    
+
+
 # ------- INIT ------- #
-try:
-    arg = sys.argv[1]
-except IndexError:
-    print("""Script Execution:
-$ python generateData.py <number> <cancel>
-    where <number> is the number of XMLs to create
-    and <cancel> is the number of reservations to cancel""")
-    exit()
+if __name__ == "__main__":
+    try:
+        arg = sys.argv[1]
+    except IndexError:
+        print("""Script Execution:
+$ python generateData.py <number> <cancel> "sendmongo" 
+    <number> is the number of XMLs to create
+    <cancel> is the number of reservations to cancel (optional)
+    sendmongo is the option to run the convert.py script and send data to mongoDB (optional)""")
+        sys.exit()
+    
+    # do some initial checks
+    initialChecks()
 
-# delete older resources
-deleteResources()
-print("Deleting old resources...")
+    # delete older resources
+    deleteResources()
+    print("Deleting old resources...")
 
-# Create XMLs with random data in it
-print("Creating XMLs...")
-for i in range(1, int(sys.argv[1])):
-    createXMLfiles()
+    # Create XMLs with random data in it
+    print("Creating XMLs...")
+    for i in range(1, int(sys.argv[1])):
+        createXMLfiles()
 
-# POST it to BaseX
-print("POSTing XMLs to BaseX...")
-POSTreservations()
+    # POST it to BaseX
+    eta = (int(sys.argv[1]) * 45) / 1000
+    eta = round(eta / 60)
+    print("POSTing XMLs to BaseX... (ETA: " + str(eta) + " minutes)")
+    POSTreservations()
 
-
-# Cancel some random reservations
-print("Cancelling reservations...")
-try:
-    for cancel in range(1, int(sys.argv[2])):
-        randomCancel()
-except IndexError:
-    print("No parameter was given to cancel reservations")
-    print("Want to cancel 25% of the reservations? (y/N)")
-    if (input() == "y"):
-        print("Cancelling 25% of the reservations...")
-        for cancel in range(1, int(sys.argv[1])//4):
+    # Cancel some random reservations
+    eta = (int(sys.argv[1]) * 45) / 1000
+    eta = round(eta / 60)
+    print("Cancelling reservations... (ETA: " + str(eta) + " minutes)")
+    try:
+        for cancel in range(1, int(sys.argv[2])):
             randomCancel()
-
-# ask the user if he wants to export the database
-try:
-    if sys.argv[3]:
-        exportData()
-    else:
-        print("Do you want to export the database? (y/N)")
+    except IndexError:
+        print("No parameter was given to cancel reservations")
+        print("Want to cancel 25% of the reservations? (y/N)")
         if (input() == "y"):
+            print("Cancelling 25% of the reservations...")
+            for cancel in range(1, int(sys.argv[1])//4):
+                randomCancel()
+
+    # ask the user if he wants to export the database
+    # only if the user didnt specify that he wants to send the data to mongoDB
+    try:
+        if sys.argv[3]:
+            exportData()
+        else:
+            print("Do you want to export the database? (y/N)")
+            if (input() == "y"):
+                print("Exporting database...")
+                exportData()
+                
+    except IndexError:
+        print("Do you want to export the database? (y/N)")
+        input = input()
+        if (input == "y"):
             print("Exporting database...")
             exportData()
-            
-except IndexError:
-    print("Do you want to export the database? (y/N)")
-    input = input()
-    if (input == "y"):
-        print("Exporting database...")
-        exportData()
-    else:
-        print("Database not exported!")
-        print("Exiting...")
+        else:
+            print("Database not exported!")
+            print("Exiting...")
 
-try:
-    if sys.argv[3] == "rui":
-        print("Running script convert.py...")
-        exec(open("convert.py").read())
-except IndexError:
-    print("No parameter was given to send data to Mongo")
+    try:
+        if sys.argv[3] == "sendmongo":
+            print("Running script convert.py...")
+            exec(open("convert.py").read())
+    except IndexError:
+        print("No parameter was given to send data to Mongo")
